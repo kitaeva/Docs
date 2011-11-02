7ce8abb768fd924607cd38979500c3d9a4c21e87
Elufimov Michael <elufimov@gmail.com>
2011-11-02 14:57:43 +0400

    Основной класс страницы пользователя

---
+++ pages/user_page.rb
@@ -0,0 +1,80 @@
+class UserPage < BrowserContainer
+
+  def goto_my_data
+    my_data_button.click
+  end
+
+  def goto_my_contact
+    my_contact_button.click
+  end
+
+  def goto_my_company
+    my_company_button.click
+  end
+
+  def contact_invite?
+    contact_invite.exist?
+  end
+
+  def company_invite?
+    company_invite.exist?
+  end
+
+  def my_contact_button_activ?
+    if my_contact_button_activ.exist? == false
+      return true
+    else
+      return false
+    end
+  end
+
+  def my_company_button_activ?
+    if my_company_button_activ.exist? == false
+      return true
+    else
+      return false
+    end
+  end
+
+  def my_data_button_activ?
+    if my_data_button_activ.exist? == false
+      return true
+    else
+      return false
+    end
+  end
+
+  private
+
+  def my_data_button
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[1]/span")
+  end
+
+  def my_contact_button
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[2]/span")
+  end
+
+  def my_company_button
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[2]/span")
+  end
+
+  def contact_invite
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[2]/span/a/i[2]")
+  end
+
+  def company_invite
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[2]/span/a/i[3]")
+  end
+
+  def my_data_button_activ
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[1]/span/a")
+  end
+
+  def my_contact_button_activ
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[2]/span/a")
+  end
+
+  def my_company_button_activ
+    @browser.element(:xpath => ".//*[@id='left_column_content']/div/div/ul/li[2]/span/a")
+  end
+end
