cf7f0c0b18f80c0eb520d11521836378d8762367
Elufimov Michael <elufimov@gmail.com>
2011-11-07 11:27:47 +0400

    Описание всех элементов

--- pages/login_page.rb
+++ pages/login_page.rb
@@ -5,21 +5,66 @@ module Login
     login_field.send_keys user
     password_field.send_keys pass
     auth_button.click
-    Watir::Wait.until {profile_button.exist?}
+  end
+
+  def get_login_text
+    login_field.text
+  end
+
+  def get_password_text
+    password_field.text
   end
 
   private
 
   def login_field
-    @browser.frame(:index, 1).element_by_xpath(".//*[@id='email']")
+    @browser.frame(:index => 1).text_field(:id => "email")
   end
 
   def password_field
-    @browser.frame(:index, 1).element_by_xpath(".//*[@id='password']")
+    @browser.frame(:index => 1).text_field(:id => "password")
   end
 
   def auth_button
-    @browser.frame(:index, 1).element_by_xpath(".//*[@id='popup_container']/div[2]/form/fieldset[2]/div[2]/button")
+    @browser.frame(:index => 1).button(:tag_name => "Войти")
+  end
+
+  def remember_checkbox
+    @browser.frame(:index => 1).checkbox(:name => "remember_me")
+  end
+
+  def remember_button
+    @browser.frame(:index => 1).button(:tag_name => "Вспомнить пароль")
+  end
+
+  def goto_registration
+    @browser.frame(:index => 1).button(:tag_name => "Зарегистрируйся сейчас!")
+  end
+
+end
+
+module Registration
+
+  private
+
+  def first_name_field
+    @browser.frame(:index => 1).text_field(:id => "first_name")
+  end
+
+  def last_name_field
+    @browser.frame(:index => 1).text_field(:id => "first_name")
+  end
+
+  def email_field
+    @browser.frame(:index => 1).text_field(:id => "email")
+  end
+
+  def password_field
+    @browser.frame(:index => 1).text_field(:id => "password")
+  end
+
+  def registration_button
+    @browser.frame(:index => 1).button(:class_name => "b-popup-form-button b-popup-form-button--submit")
   end
 
 end
